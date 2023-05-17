00491f6c55d43bff9f40a5139e5f9e0f5b7f7282
Donald Hamel <donald.hamel@bell.ca>
2023-05-16 21:18:15 -0400

    log request

--- server.go
+++ server.go
@@ -11,6 +11,7 @@ import (
 func startServer() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s|%s %s\n", r.RemoteAddr, r.Method, r.URL.Path)
 		fmt.Fprintf(w, "Hello, TLS user! Your config: %+v", r.TLS)
 	})
 	log.Fatal(http.Serve(autocert.NewListener(*cname), mux))
